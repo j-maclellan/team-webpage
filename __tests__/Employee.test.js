da6f2c16eaba42f162a3a6996b5abc3a6e4e964f
Laertes <maclellanj15@gmail.com>
2022-02-21 12:00:46 -0600

    employee testing

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,11 @@
+const Employee = require("../lib/Employee");
+
+jest.mock("../lib/Employee");
+
+test("creates a new player object", () => {
+    const employee = new Employee("Steve");
+
+    expect(employee.name).toBe("Steve");
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toStrictEqual(expect.any(String));
+});
